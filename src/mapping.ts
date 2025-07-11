b4d8a7db1ed67a0b4a175400130ac8329223e1fc
SohZHong <sohzhehong09@gmail.com>
2025-07-11 15:21:17 +0800

    add: schema and mappings for schedule

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,13 +6,20 @@ export const mapping: Mapping = {
     typeIds: [Id.Id('5c6e72fb-8340-47c0-8281-8be159ecd495')],
     properties: {
       name: Id.Id('a126ca53-0c8e-48d5-b888-82c734c38935'),
-      description: Id.Id('9b1f76ff-9711-404c-861e-59dc3fa7d037'),
+      address: Id.Id('0d625978-4b3c-4b57-a86f-de45c997c73c'),
     },
   },
-  Project: {
-    typeIds: [Id.Id('484a18c5-030a-499c-b0f2-ef588ff16d50')],
+  Schedule: {
+    typeIds: [Id.Id('4d876b81-787e-41fc-ab5d-075d4da66a3f')],
     properties: {
       name: Id.Id('a126ca53-0c8e-48d5-b888-82c734c38935'),
+      description: Id.Id('9b1f76ff-9711-404c-861e-59dc3fa7d037'),
+      endTime: Id.Id('c3445f6b-e2c0-4f25-b73a-5eb876c4f50c'),
+      startTime: Id.Id('2d696bf0-510f-403e-985b-8cd1e73feb9b'),
+      location: Id.Id('95d77002-1faf-4f7c-b7de-b21a7d48cda0'),
+    },
+    relations: {
+      address: Id.Id('5c6e72fb-8340-47c0-8281-8be159ecd495'),
     },
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,14 @@
 import { Entity, Type } from '@graphprotocol/hypergraph';
 export class Address extends Entity.Class<Address>('Address')({
   name: Type.Text,
-  description: Type.Text,
+  address: Type.Text,
 }) {}
 
-export class Project extends Entity.Class<Project>('Project')({
+export class Schedule extends Entity.Class<Schedule>('Schedule')({
   name: Type.Text,
+  description: Type.Text,
+  endTime: Type.Date,
+  startTime: Type.Date,
+  location: Type.Text,
+  address: Type.Relation(Address),
 }) {}
